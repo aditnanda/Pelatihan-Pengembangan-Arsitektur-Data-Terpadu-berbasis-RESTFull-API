12d661c138e59fc08da272ab63ff853dc26849b4
aditnanda <aditya.nanda0030@gmail.com>
2022-04-26 09:03:49 +0700

    refresh to null

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -65,11 +65,16 @@ class ComposerAutoloaderInit8b64d67bf120557447b54b124038e10d
     }
 }
 
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
 function composerRequire8b64d67bf120557447b54b124038e10d($fileIdentifier, $file)
 {
     if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
         $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
